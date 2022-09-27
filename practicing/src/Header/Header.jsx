12d70c81a825ce525ae237eb4bf62b83f653d867
Jéssica Garcia <jessica.rodriguesgarcia@gmail.com>
2022-09-27 18:02:03 -0300

    feat: create header

---
+++ practicing/src/Header/Header.jsx
@@ -0,0 +1,9 @@
+import styles from '../Header/Header.module.css';
+
+export function Header () {
+    return (
+        <header className={styles.header}>
+            <h1>Testando header</h1>
+        </header>
+    )
+}
