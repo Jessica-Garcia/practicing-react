2cab420eb9620acc6fb39f1c07a68caeb4e5df9e
Jéssica Garcia <jessica.rodriguesgarcia@gmail.com>
2022-09-28 17:44:10 -0300

    refactor: change header style

--- practicing/src/components/Header/Header.jsx
+++ practicing/src/components/Header/Header.jsx
@@ -3,7 +3,13 @@ import styles from '../Header/Header.module.css';
 export function Header () {
     return (
         <header className={styles.header}>
-            <h1>Testando header</h1>
+            <a className={styles.logo} href="#">Logo</a>
+            <nav className={styles.nav}>
+                <a className={styles.green} href="#">Lorem</a>
+                <a className={styles.blue} href="#">Lorem</a>
+                <a className={styles.blue} href="#">Lorem</a>
+                <a className={styles.blue} href="#">Lorem</a>
+            </nav>
         </header>
     )
 }
