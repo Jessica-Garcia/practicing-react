a82535b9a227c3d2b913c008500529a8624308ca
Jéssica Garcia <jessica.rodriguesgarcia@gmail.com>
2022-09-28 17:42:03 -0300

    feat: create page top section

--- practicing/src/App.jsx
+++ practicing/src/App.jsx
@@ -1,10 +1,13 @@
-import { Header } from "./components/Header/Header"
+import { Header } from "./components/Header/Header";
+import { TopSection } from "./components/TopSection/TopSection";
+import './global.css';
 
 function App() {
 
   return (
     <div>
       <Header/>
+      <TopSection/>
     </div>
   )
 }
