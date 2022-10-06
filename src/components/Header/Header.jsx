69fe48b055f278382eb1aa774c066016eaba647e
Jéssica Garcia <jessica.rodriguesgarcia@gmail.com>
2022-10-06 10:19:28 -0300

    refactor: test styled-components lib

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import styles from '../Header/Header.module.css';
-import { ButtonLinks } from '../ButtonLinks/ButtonLinks';
+// import { ButtonLinks } from '../ButtonLinks/ButtonLinks';
+import { Button } from "../ButtonLinks/ButtonLinksStyle"
 
 export function Header () {
     return (
         <header className={styles.header}>
             <a className={styles.logo} href="#">Logo</a>
             <nav className={styles.nav}>
-                <ButtonLinks content={'Link1'} />
-                <ButtonLinks content={'Link2'} />
-                <ButtonLinks content={'Link3'} />
-                <ButtonLinks content={'Link4'} />
+                <Button as="a" href="#">Link 1</Button>
+                <Button as="a" href="#" blue>Link</Button>
+                <Button as="a" href="#" blue>Link</Button>
+                <Button as="a" href="#" blue>Link</Button>
             </nav>
         </header>
     )
