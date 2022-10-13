439df2feb7941b7664038ecfff56ba7a03ed8d8e
Jéssica Garcia <jessica.rodriguesgarcia@gmail.com>
2022-10-13 16:56:15 -0300

    refactor: change author information

--- src/Pages/Feed/Feed.jsx
+++ src/Pages/Feed/Feed.jsx
@@ -21,11 +21,11 @@ const posts = [
     {
         id: 2,
         author: {
-            avatarUrl: "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60&h=500",
-            name: 'Joan',
+            avatarUrl: "https://images.unsplash.com/photo-1499887142886-791eca5918cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60&h=500",
+            name: 'Mary',
             role: 'Yoga Instructor'
         },
-        publishedAt: new Date('2022-10-10 20:00:00'),
+        publishedAt: new Date('2022-10-9 20:00:00'),
         content: [
             {type: 'paragraph', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
             {type: 'paragraph', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
@@ -35,11 +35,11 @@ const posts = [
     {
         id: 3,
         author: {
-            avatarUrl: "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60&h=500",
-            name: 'Joan',
+            avatarUrl: "https://images.unsplash.com/photo-1598966739654-5e9a252d8c32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTF8TWpZa3RDVEZtWWN8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60&h=500",
+            name: 'Mick',
             role: 'Yoga Instructor'
         },
-        publishedAt: new Date('2022-10-10 20:00:00'),
+        publishedAt: new Date('2022-10-8 20:00:00'),
         content: [
             {type: 'paragraph', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
             {type: 'paragraph', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
